e6711448e74482ea1ce7fcd0ffa93c3e35f13731
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2017-10-02 14:53:00 +0200

    Fix build

--- deps/build.jl
+++ deps/build.jl
@@ -1,8 +1,11 @@
-using BinDeps
-using Compat
+let path = "libasdk."*Base.Libdl.dlext,
+    dlopen_e = (VERSION >= v"0.4.0-dev+3844" ? Base.Libdl : Base).dlopen_e
+    if dlopen_e(path) == C_NULL
+        error("Unable to load dynamic library \"$path\".\n\nPlease (re)install Alpao SDK, re-run \`Pkg.build(\"Alpao\")\`, and restart Julia.")
+    end
 
-@BinDeps.setup
-
-libalpao = library_dependency("libasdk", aliases = ["ASDK"], runtime = true)
-
-@BinDeps.install Dict(:libalpao => :DLL)
+    open(joinpath(dirname(@__FILE__),"deps.jl"), "w") do io
+        write(io, "# This is an auto-generated file; do not edit.\n\n")
+        write(io, "const DLL = \"$path\"\n")
+    end
+end
