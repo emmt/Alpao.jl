c2c155b88168897729ccbf7659e7374920a13d98
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2017-11-14 08:38:17 +0100

    Define dll extension

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,7 @@
-let path = "libasdk."*Base.Libdl.dlext,
-    dlopen_e = (VERSION >= v"0.4.0-dev+3844" ? Base.Libdl : Base).dlopen_e
+let dlext = (VERSION >= v"0.4.0-dev+3844" ? Base.Libdl : Base).dlext,
+    dlopen_e = (VERSION >= v"0.4.0-dev+3844" ? Base.Libdl : Base).dlopen_e,
+    path = "libasdk."*dlext
+
     if dlopen_e(path) == C_NULL
         error("Unable to load dynamic library \"$path\".\n\nPlease (re)install Alpao SDK, re-run \`Pkg.build(\"Alpao\")\`, and restart Julia.")
     end
