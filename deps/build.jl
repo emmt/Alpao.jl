eefafad303d6574b470b78561ad1656a9ffe028c
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2019-02-16 00:09:34 +0100

    Use @__DIR__ macro

--- deps/build.jl
+++ deps/build.jl
@@ -1,12 +1,11 @@
-let dlext = (Base.Libdl).dlext,
-    dlopen_e = (Base.Libdl).dlopen_e,
-    path = "libasdk."*dlext
+using Libdl
+let path = "libasdk."*dlext
 
     if dlopen_e(path) == C_NULL
         error("Unable to load dynamic library \"$path\".\n\nPlease (re)install Alpao SDK, re-run \`Pkg.build(\"Alpao\")\`, and restart Julia.")
     end
 
-    open(joinpath(dirname(@__FILE__),"deps.jl"), "w") do io
+    open(joinpath(@__DIR__, "deps.jl"), "w") do io
         write(io, "# This is an auto-generated file; do not edit.\n\n")
         write(io, "const DLL = \"$path\"\n")
     end
